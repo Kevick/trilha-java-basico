39be0339bd0bf8e54bbb18aa5c7b63a26509169d
Tungs <jairneto180801@gmail.com>
2024-07-05 23:58:20 -0300

    teste

--- codigo-fonte/tipos-variaveis/src/TiposVariaveis.java
+++ codigo-fonte/tipos-variaveis/src/TiposVariaveis.java
@@ -1,8 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-      byte numero1 = 100;
+      byte numero1 = -128;
       short  numero2=numero1;
+      final double VALOR_DE_PI = 3.14;
       numero1 = (byte) numero2;
       System.out.println(numero1);
+      System.out.println(VALOR_DE_PI);
     }
 }
