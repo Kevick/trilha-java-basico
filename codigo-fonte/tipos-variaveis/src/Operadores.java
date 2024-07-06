431e911ab2cd76bd1b609fb7abf029a581a9b181
Tungs <jairneto180801@gmail.com>
2024-07-06 00:13:38 -0300

    operadores

--- codigo-fonte/tipos-variaveis/src/Operadores.java
+++ codigo-fonte/tipos-variaveis/src/Operadores.java
@@ -1,5 +1,9 @@
 public class Operadores {
     public static void main(String[] args) {
-        
+        int numero = 5;
+        numero = -numero;
+        System.out.println(numero);
+        numero = numero * -1; //usar numero negativo multiplicado por -1 para positivar o mesmo.
+        System.out.println(numero);
     }
 }
